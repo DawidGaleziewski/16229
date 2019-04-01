b78692b39b4de7255d33ae0fbc5ea580d3de8a50
Dawid Galeziewski <Dawid.Galeziewski@ivanti.com>
2019-04-01 18:15:06 +0200

    added functions requested

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let liczbaPierwsza = 12;
+let liczbaDruga = 23;
+
+// In es5
+function sumEs5(x, y){
+    let suma = x + y;
+    return suma;
+}
+
+// In es6 arrow functions
+let sumEs6 = (x, y) => {return x + y};
